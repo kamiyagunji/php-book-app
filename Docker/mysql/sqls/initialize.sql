3a562e658b0bcd03d8d2dc118412a206d323096c
kamiyagunji <kamiyagunji@icloud.com>
2019-06-10 02:41:34 +0900

    Fixed SQL

--- Docker/mysql/sqls/initialize.sql
+++ Docker/mysql/sqls/initialize.sql
@@ -13,20 +13,31 @@ CREATE TABLE `answers` (
 
 CREATE TABLE `questions` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-   `user_id` int(10) unsigned NOT NULL,
-   `body` varchar(255) NOT NULL,
-   `created` datetime NOT NULL,
-   `modified` datetime NOT NULL,
-   PRIMARY KEY (`id`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `user_id` int(10) unsigned NOT NULL,
+    `body` varchar(255) NOT NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
-   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-   `username` varchar(16) NOT NULL,
-   `password` varchar(255) NOT NULL,
-   `nickname` varchar(32) NOT NULL,
-   `created` datetime NOT NULL,
-   `modified` datetime NOT NULL,
-   PRIMARY KEY (`id`),
-   UNIQUE KEY `username` (`username`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(16) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `nickname` varchar(32) NOT NULL,
+    `created` datetime NOT NULL,
+    `modified` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`)
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO questions (user_id, body, created, modified)
+VALUES
+    (1, '普段PHPのフレームワークって何使ってますか？', now(), now()),
+    (1, 'PHPにあったらいいなと思う機能ってありますか？', now(), now()),
+    (1, 'みなさんはいつからPHPを書いてますか？', now(), now());
+
+INSERT INTO answers (question_id, user_id, body, created, modified)
+VALUES
+    (3, 1, '私はLaravelです！あとたまにYiiを使っています！', now(), now()),
+    (3, 1, '僕はCakePHP！', now(), now());
